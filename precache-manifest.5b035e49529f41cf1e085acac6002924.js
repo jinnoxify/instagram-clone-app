9bcfe68573e3ab35d2f7e96b61ee631c0a13f8f3
jinnoxify <oscarpereznavarro1997@gmail.com>
2020-09-25 23:17:31 -0500

    Updates

---
+++ precache-manifest.5b035e49529f41cf1e085acac6002924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5799deb06eb872f7554a0f1c86cad3df",
+    "url": "/instagram-clone-app/index.html"
+  },
+  {
+    "revision": "6176186e553fa5210beb",
+    "url": "/instagram-clone-app/static/css/main.21f863b8.chunk.css"
+  },
+  {
+    "revision": "29eb893b15ef17b4c7db",
+    "url": "/instagram-clone-app/static/js/2.e09ff007.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instagram-clone-app/static/js/2.e09ff007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6176186e553fa5210beb",
+    "url": "/instagram-clone-app/static/js/main.472cd483.chunk.js"
+  },
+  {
+    "revision": "f0055f563548b72d5674",
+    "url": "/instagram-clone-app/static/js/runtime-main.896ff18d.js"
+  }
+]);
