011c9ffd86616f48a3f564199c2882b7c1a1c36b
jinnoxify <oscarpereznavarro1997@gmail.com>
2020-10-21 21:16:44 -0500

    updated firebase config

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyAMWPgxDSYMUbi5ontpY3Wrr97Y_WE0-cc",
-  authDomain: "instagram-clone-project-765fc.firebaseapp.com",
-  databaseURL: "https://instagram-clone-project-765fc.firebaseio.com",
-  projectId: "instagram-clone-project-765fc",
-  storageBucket: "instagram-clone-project-765fc.appspot.com",
-  messagingSenderId: "303612171239",
-  appId: "1:303612171239:web:e55dfe20b9257b8eeb2342",
+  apiKey: "AIzaSyBxWvUfD_9YCAqjf705NhGS_DKL-tzmEfE",
+  authDomain: "instagram-clone-app-f9be5.firebaseapp.com",
+  databaseURL: "https://instagram-clone-app-f9be5.firebaseio.com",
+  projectId: "instagram-clone-app-f9be5",
+  storageBucket: "instagram-clone-app-f9be5.appspot.com",
+  messagingSenderId: "456351991307",
+  appId: "1:456351991307:web:d2e4990d161c874e23690e",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
